0a05c5b48b4ade1f7871b82dcdfe586b2ca83038
yann1906 <47141147+yann1906@users.noreply.github.com>
2021-12-12 12:02:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import panda as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 DATA = pd.read_csv('ETHUSDT_data.csv')
 st.subheader('Raw data')

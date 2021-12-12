0e35c77c61e5d384ed840758a598efbf6d8419e4
yann1906 <47141147+yann1906@users.noreply.github.com>
2021-12-12 12:11:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import zipfile
 
-DATA = pd.read_csv('ETHUSDT_data.csv')
-st.subheader('Raw data')
-st.write(DATA)
+zf = zipfile.ZipFile('https://github.com/yann1906/Deep_learning_webapp/blob/main/ETHUSDT_data.zip') 
+zf.namelist() 
+# df = pd.read_csv(zf.open('intfile.csv'))
+
+# DATA = pd.read_csv('ETHUSDT_data.csv')
+# st.subheader('Raw data')
+# st.write(DATA)

f519c70d7f2bc21e8f6513d4ad09017f449eae2f
yann1906 <47141147+yann1906@users.noreply.github.com>
2021-12-12 12:29:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,18 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import zipfile
+from urllib.request import urlopen
+from io import BytesIO
+from zipfile import ZipFile
 
-zf = zipfile.ZipFile('https://github.com/yann1906/Deep_learning_webapp/blob/main/ETHUSDT_data.zip') 
+URL = 'https://github.com/yann1906/Deep_learning_webapp/blob/main/ETHUSDT_data.zip'
+
+def download_and_unzip(url, extract_to='.'):
+    http_response = urlopen(url)
+    zipfile = ZipFile(BytesIO(http_response.read()))
+    zipfile.extractall(path=extract_to)
+    
+zf = zipfile.ZipFile('ETHUSDT_data.csv') 
 zf.namelist() 
 # df = pd.read_csv(zf.open('intfile.csv'))
 
